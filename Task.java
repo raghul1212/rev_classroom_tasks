e239e05ac191d4fbe8e51cb84d4fa284090bd46e
raghul1212 <72686773+raghul1212@users.noreply.github.com>
2021-08-16 22:39:39 +0530

    Add files via upload
    16-08-2021 sum of salaries using streams

---
+++ Task.java
@@ -0,0 +1,60 @@
+package com.revature.practice.problems;
+
+
+
+	import java.util.Arrays;
+	import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+	public class Task {
+	 public static void main(String[] args) {
+	  List < Employee > employees = Arrays.asList(new Employee(1, 1000, "Chandra Shekhar", 6000),
+	   new Employee(1, 1000, "Rajesh", 8000), new Employee(1, 1004,"Rahul", 9000),
+	   new Employee(1, 1001, "Suresh", 12000),
+	   new Employee(1, 1004, "Satosh", 8500));
+	  Employee e=new Employee();
+	  
+	 int total =e.getSalarySum(employees);
+	  System.out.println(total);
+	 }
+
+	}
+	
+	
+	 class Employee {
+		 private int id;
+		 private int deptId;
+		 private String name;
+		 private int salary;
+			Employee(){
+				
+			}
+					 
+		 public Employee(int id, int deptId, String name, int salary) {
+		  super();
+		  this.id = id;
+		  this.deptId = deptId;
+		  this.name = name;
+		  this.salary = salary;
+		 }
+		
+		public int getId() {
+		  return id;
+		 }
+		 public String getName() {
+		  return name;
+		 }
+		 public int getSalary() {
+		  return salary;
+		 }
+		 public int getDeptId() {
+		  return deptId;
+		 }
+		 
+		 public int getSalarySum(List<Employee> employees) {
+			int result=employees.stream().map(p->p.getSalary()).reduce((a,b)->a+b).get();
+				return result;
+			}
+
+		}
